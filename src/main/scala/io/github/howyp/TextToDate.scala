a1dae91c91dc96d0d24f6ee4c4f2ebd6bf1f59e0
Howy Perrin <howard@perrin.co.uk>
2015-05-10 20:16:09 +0100

    Support 'day after today' and '5 days'

--- src/main/scala/io/github/howyp/TextToDate.scala
+++ src/main/scala/io/github/howyp/TextToDate.scala
@@ -7,18 +7,28 @@ import scala.util.parsing.combinator.RegexParsers
 trait TextToDate extends RegexParsers {
   def now: LocalDate
 
+  def integer: Parser[Int] = "\\d+".r ^^ (_.toInt)
+
   def today: Parser[LocalDate] = ("now" | "today") ^^^ now
   def tomorrow: Parser[LocalDate] = "tomorrow" ^^^ now.plusDays(1)
   def yesterday: Parser[LocalDate] = "yesterday" ^^^ now.minusDays(1)
 
-  def daysBeforeAfter: Parser[Int] = "day before" ^^^ -1
-
-  def relativeDay: Parser[LocalDate] = opt(daysBeforeAfter) ~ (today | tomorrow | yesterday) map {
-    case None ~ date               => date
-    case Some(dayIncrement) ~ date => date plusDays dayIncrement
+  def daysBeforeAfter: Parser[Int] = opt(integer) ~ "days?".r ~ opt("before"| "after") map {
+    case None        ~ _ ~ Some("before") => - 1
+    case None        ~ _ ~ Some("after")  => + 1
+    case Some(count) ~ _ ~ None           => + count
   }
 
-  def parse(text: String): LocalDate = parse(relativeDay, text).get
+  def relativeDay: Parser[LocalDate] = opt(daysBeforeAfter) ~ opt(today | tomorrow | yesterday) map {
+    case None               ~ Some(date) => date
+    case Some(dayIncrement) ~ Some(date) => date plusDays dayIncrement
+    case Some(dayIncrement) ~ None       => now  plusDays dayIncrement
+  }
+
+  def parse(text: String): LocalDate = parse(relativeDay, text) match {
+    case Success(r, _) => r
+    case Failure(msg, _) => throw new RuntimeException(msg)
+  }
 }
 
 object TextToDate extends TextToDate {
--- src/test/scala/TextToDateSpec.scala
+++ src/test/scala/TextToDateSpec.scala
@@ -5,6 +5,7 @@ import org.scalatest.{Matchers, WordSpec}
 class TextToDateSpec extends WordSpec with Matchers {
   val exampleNow = LocalDate.parse("2015-05-09")
   val examplePlusADay = LocalDate.parse("2015-05-10")
+  val examplePlusFiveDays = LocalDate.parse("2015-05-14")
   val exampleMinusADay = LocalDate.parse("2015-05-08")
   val exampleMinusTwoDays = LocalDate.parse("2015-05-07")
   val parse = new TextToDate { val now = exampleNow }.parse _
@@ -25,6 +26,12 @@ class TextToDateSpec extends WordSpec with Matchers {
     }
     "understand 'day before today'" in {
       parse("day before today") should be (exampleMinusADay)
+    }
+    "understand 'day after today'" in {
+      parse("day after today") should be (examplePlusADay)
+    }
+    "understand '5 days'" in {
+      parse("5 days") should be (examplePlusFiveDays)
     }
   }
 }
