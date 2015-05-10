2ccffc7d19fe7843b14760400b473388c9a14821
Howy Perrin <howard@perrin.co.uk>
2015-05-10 07:15:17 +0100

    Support 'today' and 'day before yesterday'

--- src/main/scala/io/github/howyp/TextToDate.scala
+++ src/main/scala/io/github/howyp/TextToDate.scala
@@ -7,7 +7,16 @@ import scala.util.parsing.combinator.RegexParsers
 trait TextToDate extends RegexParsers {
   def now: LocalDate
 
-  def relativeDay: Parser[LocalDate] = "now" ^^^ now | "tomorrow" ^^^ now.plusDays(1)
+  def today: Parser[LocalDate] = ("now" | "today") ^^^ now
+  def tomorrow: Parser[LocalDate] = "tomorrow" ^^^ now.plusDays(1)
+  def yesterday: Parser[LocalDate] = "yesterday" ^^^ now.minusDays(1)
+
+  def daysBeforeAfter: Parser[Int] = "day before" ^^^ -1
+
+  def relativeDay: Parser[LocalDate] = opt(daysBeforeAfter) ~ (today | tomorrow | yesterday) map {
+    case None ~ date               => date
+    case Some(dayIncrement) ~ date => date plusDays dayIncrement
+  }
 
   def parse(text: String): LocalDate = parse(relativeDay, text).get
 }
--- src/test/scala/TextToDateSpec.scala
+++ src/test/scala/TextToDateSpec.scala
@@ -4,15 +4,27 @@ import org.scalatest.{Matchers, WordSpec}
 
 class TextToDateSpec extends WordSpec with Matchers {
   val exampleNow = LocalDate.parse("2015-05-09")
-  val exampleTomorrow = LocalDate.parse("2015-05-10")
+  val examplePlusADay = LocalDate.parse("2015-05-10")
+  val exampleMinusADay = LocalDate.parse("2015-05-08")
+  val exampleMinusTwoDays = LocalDate.parse("2015-05-07")
   val parse = new TextToDate { val now = exampleNow }.parse _
 
   "TextToDate" should {
-    "understand 'now'" in {
+    "understand words for the current day" in {
       parse("now") should be (exampleNow)
+      parse("today") should be (exampleNow)
     }
     "understand 'tomorrow'" in {
-      parse("tomorrow") should be (exampleTomorrow)
+      parse("tomorrow") should be (examplePlusADay)
+    }
+    "understand 'yesterday'" in {
+      parse("yesterday") should be (exampleMinusADay)
+    }
+    "understand 'day before yesterday'" in {
+      parse("day before yesterday") should be (exampleMinusTwoDays)
+    }
+    "understand 'day before today'" in {
+      parse("day before today") should be (exampleMinusADay)
     }
   }
 }
