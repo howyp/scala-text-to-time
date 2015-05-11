ce5ca9f40ab1f0e9a4651ce984f57c417c08deff
Howy Perrin <howard@perrin.co.uk>
2015-05-11 18:46:11 +0100

    Use dedicated matcher in test

--- src/test/scala/TextToDateSpec.scala
+++ src/test/scala/TextToDateSpec.scala
@@ -4,26 +4,25 @@ import org.scalatest.matchers.{MatchResult, Matcher}
 import org.scalatest.{Matchers, WordSpec}
 
 class TextToDateSpec extends WordSpec with Matchers with TextToDate {
-  val exampleNow = LocalDate.parse("2015-05-09")
-  val examplePlusADay = LocalDate.parse("2015-05-10")
-  val examplePlusFiveDays = LocalDate.parse("2015-05-14")
-  val exampleMinusADay = LocalDate.parse("2015-05-08")
-  val exampleMinusTwoDays = LocalDate.parse("2015-05-07")
+  val now = LocalDate.parse("2015-05-09")
+  val nowPlusADay = LocalDate.parse("2015-05-10")
+  val nowPlusFiveDays = LocalDate.parse("2015-05-14")
+  val nowMinusADay = LocalDate.parse("2015-05-08")
+  val nowMinusTwoDays = LocalDate.parse("2015-05-07")
 
-  val now = exampleNow
 
   "TextToDate" should {
     Map(
-      "now" -> exampleNow,
-      "today" -> exampleNow,
-      "tomorrow" -> examplePlusADay,
-      "yesterday" -> exampleMinusADay,
-      "day before yesterday" -> exampleMinusTwoDays,
-      "day before today" -> exampleMinusADay,
-      "day after today" -> examplePlusADay,
-      "5 days" -> examplePlusFiveDays,
-      "2 days before tomorrow" -> exampleMinusADay,
-      "4 days after tomorrow" -> examplePlusFiveDays
+      "now" -> now,
+      "today" -> now,
+      "tomorrow" -> nowPlusADay,
+      "yesterday" -> nowMinusADay,
+      "day before yesterday" -> nowMinusTwoDays,
+      "day before today" -> nowMinusADay,
+      "day after today" -> nowPlusADay,
+      "5 days" -> nowPlusFiveDays,
+      "2 days before tomorrow" -> nowMinusADay,
+      "4 days after tomorrow" -> nowPlusFiveDays
     ) foreach { case (sample, expected) =>
       s"understand '$sample'" in {
         parseAll(relativeDay, sample) should succeedWith (expected)
@@ -35,7 +34,12 @@ class TextToDateSpec extends WordSpec with Matchers with TextToDate {
     case Success(result, _) => MatchResult(
       matches = result == expected,
       rawFailureMessage = s"Result was '$result', expected '$expected'",
-      rawNegatedFailureMessage = ""
+      rawNegatedFailureMessage = s"Result should not have been $result"
+    )
+    case f @ (_: Failure | _: Error) => MatchResult(
+      matches = false,
+      rawFailureMessage = s"Parser returned '$f', expected '$expected'",
+      rawNegatedFailureMessage = s"Parser returned '$f', result should not have been $expected"
     )
   }
 }
